61218d20069256739dcbbad82bdd57fe3463c9f2
YKurtevVSG <135205052+YKurtevVSG@users.noreply.github.com>
2023-06-15 17:30:22 +0300

    Small fix

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -2,7 +2,6 @@ export const msalConfig = {
     auth: {
         clientId: "86ceffd4-8632-4677-bbb6-e7badafa26ec",
         authority: "https://login.microsoftonline.com/50ae1bf7-d359-4aff-91ac-b084dc52111e",
-        redirectUri: "http://localhost:5173/",
         navigateToLoginRequestUrl: false
     },
     cache: {
