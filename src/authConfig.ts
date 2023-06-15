7ff144838bd571606215449161c882319b1d1402
YKurtevVSG <135205052+YKurtevVSG@users.noreply.github.com>
2023-06-15 17:34:29 +0300

    Small fix

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,4 +1,6 @@
-export const msalConfig = {
+import { Configuration, PublicClientApplication } from "@azure/msal-browser";
+
+export const msalConfig: Configuration = {
     auth: {
         clientId: "86ceffd4-8632-4677-bbb6-e7badafa26ec",
         authority: "https://login.microsoftonline.com/50ae1bf7-d359-4aff-91ac-b084dc52111e",
@@ -17,3 +19,5 @@ interface LoginRequest {
 export const loginRequest: LoginRequest = {
     scopes: ["email profile openid api://86ceffd4-8632-4677-bbb6-e7badafa26ec/Files.Read"]
 };
+
+export const instance = new PublicClientApplication(msalConfig);
